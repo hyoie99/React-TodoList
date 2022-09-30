a3cda4c4d873c5306a3c9dca6760f34f2bb2e37e
hyen <80624327+hyoie99@users.noreply.github.com>
2022-09-30 17:48:54 +0900

    make simple 'todo list'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const inputTODO = (event) => {
+    setToDo(event.target.value);
+    // console.log(toDo);
+  };
+  const submitEvent = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+    if (toDo === "") {
+      return;
+    } else {
+      setToDos((currentList) => [...currentList, toDo]);
+      setToDo("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1> ToDo List </h1>
+      <form onSubmit={submitEvent}>
+        <input
+          value={toDo}
+          onChange={inputTODO}
+          type="text"
+          placeholder="What to do?"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, key) => (
+          <li key={key}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
